4ab281ffec70054b8f3e832de4242b89d0988ff6
matnedjer <matnedjer@foi.hr>
2014-11-22 14:44:54 +0100

    Dodana pravila igre.

---
+++ TestMathFun/src/air/testmathfun/Pravila.java
@@ -0,0 +1,16 @@
+package air.testmathfun;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Pravila extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pravila);
+	}
+	
+}
